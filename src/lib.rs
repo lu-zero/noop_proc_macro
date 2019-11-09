7ccd2683a64b61f27772fddd70a9dc80fea315ac
Luca Barbato <lu_zero@gentoo.org>
2019-11-09 10:49:10 +0900

    Add hawktracer no-ops

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 extern crate proc_macro;
 
+use proc_macro::*;
+
 #[proc_macro_derive(Serialize, attributes(serde))]
 pub fn serialize(_items: proc_macro::TokenStream) -> proc_macro::TokenStream {
     proc_macro::TokenStream::new()
@@ -10,3 +12,12 @@ pub fn deserialize(_items: proc_macro::TokenStream) -> proc_macro::TokenStream {
     proc_macro::TokenStream::new()
 }
 
+#[proc_macro_attribute]
+pub fn hawktracer(_args: TokenStream, input: TokenStream) -> TokenStream {
+    input
+}
+
+#[proc_macro]
+pub fn scoped_tracepoint(_item: TokenStream) -> TokenStream {
+    TokenStream::new()
+}
